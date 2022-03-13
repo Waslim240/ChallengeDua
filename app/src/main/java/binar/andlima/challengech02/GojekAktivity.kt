ff8dd5a7ae9c150c0b3715fc63f5e2d1c585fb76
Waslim <waslim.liem19240@student.unsika.ac.id>
2022-03-13 14:34:09 +0700

    menambahkan project ke github

---
+++ app/src/main/java/binar/andlima/challengech02/GojekAktivity.kt
@@ -0,0 +1,11 @@
+package binar.andlima.challengech02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GojekAktivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.aktivity_gojek)
+    }
+}
